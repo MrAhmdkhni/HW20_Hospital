5542c9d8656e0e8bd7371399ea3e9195a06ab212
Amir <popularamirhosein@gmail.com>
2022-11-05 00:39:19 +0330

    add constructor with parameter and default

--- src/main/java/entity/Admin.java
+++ src/main/java/entity/Admin.java
@@ -9,4 +9,10 @@ public class Admin extends Person {
     @OneToOne(mappedBy = "admin") // har admin bara y clinic
     private Clinic clinic;
 
+    public Admin(String firstname, String lastname, String nationalCode, String username, String password) {
+        super(firstname, lastname, nationalCode, username, password);
+    }
+
+    public Admin() {
+    }
 }
