e411c1071e0f7a42ac1d1ecffced95e76586bbb5
Amir <popularamirhosein@gmail.com>
2022-11-05 00:49:43 +0330

    add constructor and toString

--- src/main/java/entity/Prescription.java
+++ src/main/java/entity/Prescription.java
@@ -14,4 +14,22 @@ public class Prescription extends BaseEntity<Long> {
 
     @ManyToOne // har prescription bar y doctor
     private Doctor doctor;
+
+    public Prescription(String description, Patient patient, Doctor doctor) {
+        this.description = description;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public Prescription() {
+    }
+
+    @Override
+    public String toString() {
+        return "Prescription {" +
+                "description = '" + description + '\'' +
+                ", patient = '" + patient.getFirstname() + " " + patient.getLastname() + '\'' +
+                ", doctor = '" + doctor.getFirstname() + " " + doctor.getLastname() + '\'' +
+                "} ";
+    }
 }
