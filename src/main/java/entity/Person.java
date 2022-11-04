24da12607d70025cd63b242f51da790a33051fe2
Amir <popularamirhosein@gmail.com>
2022-11-05 00:49:13 +0330

    add constructor and annotation column and toString

--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -2,24 +2,44 @@ package entity;
 
 import base.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class Person extends BaseEntity<Long> {
 
+    @Column(nullable = false)
     private String firstname;
+    @Column(nullable = false)
     private String lastname;
-    private String nationalCod;
+    @Column(nullable = false, unique = true)
+    private String nationalCode;
+    @Column(nullable = false, unique = true)
     private String username;
+    @Column(nullable = false)
     private String password;
 
-    public Person(String firstname, String lastname, String nationalCod, String username, String password) {
+    public Person(String firstname, String lastname, String nationalCode, String username, String password) {
         this.firstname = firstname;
         this.lastname = lastname;
-        this.nationalCod = nationalCod;
+        this.nationalCode = nationalCode;
         this.username = username;
         this.password = password;
     }
 
     public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", nationalCode='" + nationalCode + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
