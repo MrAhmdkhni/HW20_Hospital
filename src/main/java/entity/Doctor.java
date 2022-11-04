3aa013dd9f30f4ac8d271caaf5e7aaf9d29ee220
Amir <popularamirhosein@gmail.com>
2022-11-05 00:45:07 +0330

    add constructor

--- src/main/java/entity/Doctor.java
+++ src/main/java/entity/Doctor.java
@@ -3,10 +3,14 @@ package entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 public class Doctor extends Person {
 
@@ -15,4 +19,13 @@ public class Doctor extends Person {
 
     @OneToMany(mappedBy = "doctor") // har doctor chan ta prescription
     private Set<Prescription> prescriptions = new HashSet<>();
+
+    public Doctor(String firstname, String lastname, String nationalCode, String username, String password) {
+        super(firstname, lastname, nationalCode, username, password);
+    }
+
+    public Doctor() {
+    }
+
+
 }
