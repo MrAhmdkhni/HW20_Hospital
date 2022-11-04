d8ddb73b5652c3ce466a11894b9f9da745b5a68d
Amir <popularamirhosein@gmail.com>
2022-11-05 00:37:03 +0330

    create DoctorDTO class

---
+++ src/main/java/entity/DoctorDTO.java
@@ -0,0 +1,14 @@
+package entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DoctorDTO {
+
+    private String firstname;
+    private String lastname;
+}
