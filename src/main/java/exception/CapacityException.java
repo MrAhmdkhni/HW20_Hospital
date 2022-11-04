e0ffd9e102bc89ce786a9c65dcae6af770b73dfe
Amir <popularamirhosein@gmail.com>
2022-11-05 00:37:39 +0330

    create CapacityException and NotFoundClassException class

---
+++ src/main/java/exception/CapacityException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class CapacityException extends RuntimeException {
+
+    public CapacityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/exception/NotFoundClassException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class NotFoundClassException extends RuntimeException {
+
+    public NotFoundClassException(String message) {
+        super(message);
+    }
+}
